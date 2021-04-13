407b208587b56dfbc85d917394f0411a160f0a9d
molly soufrine <mollysoufrine@gmail.com>
2021-04-13 13:11:06 -0400

    commit changes before adding vue router

---
+++ book-project/src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<template>
+<header>
+    <h1></h1>
+    <nav>
+        <router-link :to="{name:'Home'}">Home</router-link>
+        <router-link :to="{name:'Books'}">Book List</router-link>
+        <router-link :to="{name:'Create'}">Add a Book</router-link>
+    </nav>
+</header>
+</template>
+
+<script>
+
+
+export default defineComponent({
+
+})
+</script>
