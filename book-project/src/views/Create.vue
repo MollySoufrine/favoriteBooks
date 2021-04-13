08a9173f681b954fbc3090538755ac8ad5b5b466
molly soufrine <mollysoufrine@gmail.com>
2021-04-13 17:03:41 -0400

    build a create.vue file to allow someone to add a book

---
+++ book-project/src/views/Create.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="create">
+        <form @submit.prevent="handleSubmit">
+            <label>Title: </label>
+            <input v-model="title" type="title" required>
+            <label>Author: </label>
+            <input v-model="author" type="author" required>
+            <label>Description</label>
+            <textarea v-model="body" required></textarea>
+            <button>Add Book</button>
+        </form>
+    </div>
+</template>
+<script>
+import { ref } from "vue"
+import { useRouter } from "vue-router"
+import { projectFirestone } from "../firebase/config"
+
+export default {
+    setup() {
+        const title = ref('')
+        const author = ref('')
+        const body = ref('')
+
+
+        const handleSubmit = async () => {
+         const book = {
+            title: title.value,
+            author: author.value,
+            body: body.value
+            }
+            const res = await projectFirestore.collection('books').add(book)
+            router.push({name: "Home"})
+        }
+
+
+ 
+
+        return {body, title, author, handleSubmit}
+
+    },
+
+}
+
+</script>
+
+<style>
+
+</style>
